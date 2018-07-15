9aaf16385ac2668c5a0bbeeea0178f82d6481702
Jeremy Friesner <jeremyf@meyersound.com>
2018-07-14 20:01:56 -0700

    v6.90 release

--- support/PseudoFlattenable.h
+++ support/PseudoFlattenable.h
@@ -39,7 +39,7 @@ public:
     */
    status_t CopyFrom(const Flattenable & copyFrom)
    {
-#ifndef WIN32  // avoids error C2027: use of undefined type 'muscle::Flattenable'
+#if !defined(MSC_VER) || (MSC_VER >= 1910)  // avoids error C2027: use of undefined type 'muscle::Flattenable'
       (void) copyFrom;
 #endif
       return B_ERROR;
