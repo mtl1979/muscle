a55e1ad134714f55fe2d65f6ef447ddaee82c8e8
Jeremy Friesner <jaf@meyersound.com>
2018-09-07 15:40:37 -0700

    v7.02 release

--- iogateway/MessageIOGateway.h
+++ iogateway/MessageIOGateway.h
@@ -244,6 +244,8 @@ protected:
    virtual bool AreOutgoingMessagesIndependent() const {return false;}
 
 private:
+   ByteBufferRef FlattenHeaderAndMessageAux(const MessageRef & msgRef) const;
+
 #ifdef MUSCLE_ENABLE_ZLIB_ENCODING
    ZLibCodec * GetCodec(int32 newEncoding, ZLibCodec * & setCodec) const;
 #endif
@@ -336,6 +338,27 @@ private:
 };
 DECLARE_REFTYPES(CountedMessageIOGateway);
 
+/** This function can be used to reduce memory usage when sending the same large
+  * Message to multiple MessageIOGateways.
+  *
+  * If you call this function on your large Message object just before you pass it off
+  * to one or more session objects for output, the Message object will be tagged with
+  * a rendezvous-point object such that only one of the gateways will have to allocate
+  * a serialized buffer and Flatten() the Message into it; the latter gateways will
+  * reuse the buffer created by the first gateway. This can be more memory-efficient
+  * than the default behavior (where each MessageIOGateway has to allocate its
+  * own separate ByteBuffer and separately flatten the Message into it).
+  *
+  * @param msg a reference the Message you are about to pass to multiple gateways.
+  */
+status_t OptimizeMessageForTransmissionToMultipleGateways(const MessageRef & msg);
+
+/** Returns true iff the given Message has had
+  * OptimizeMessageForTransmissionToMultipleGateways() called on it already.
+  * @param msg the Message to check for the presence of an optimization-tag.
+  */
+bool IsMessageOptimizedForTransmissionToMultipleGateways(const MessageRef & msg);
+
 //////////////////////////////////////////////////////////////////////////////////
 //
 // Here is a commented example of a flattened Message's byte structure, using
@@ -346,8 +369,8 @@ DECLARE_REFTYPES(CountedMessageIOGateway);
 //
 // Note that this information is only helpful if you are trying to implement
 // your own MessageIOGateway-compatible serialization/deserialization code.
-// C, C#, C++, Java, and Python programmers will have a much easier time if 
-// they use the MessageIOGateway functionality provided in the MUSCLE archive, 
+// C, C#, C++, Java, and Python programmers will have a much easier time if
+// they use the MessageIOGateway functionality provided in the MUSCLE archive,
 // rather than coding at the byte-stream level.
 //
 // The Message used in this example has a 'what' code value of 2 and the
